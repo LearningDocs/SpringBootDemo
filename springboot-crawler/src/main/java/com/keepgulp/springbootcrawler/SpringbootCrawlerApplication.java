216441c30a2347b6b91ae8507476248e4cd2350f
qiuqiu <guodq12@163.com>
2018-07-28 22:17:21 +0800

    java爬虫

---
+++ springboot-crawler/src/main/java/com/keepgulp/springbootcrawler/SpringbootCrawlerApplication.java
@@ -0,0 +1,12 @@
+package com.keepgulp.springbootcrawler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootCrawlerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootCrawlerApplication.class, args);
+    }
+}
